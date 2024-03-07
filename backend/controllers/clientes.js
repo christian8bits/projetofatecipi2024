bb87a288b2e3d6e7d740cab677458a25d54f9cd7
Christian Bits <christian8bits@gmail.com>
2024-03-07 16:50:38 -0300

    backend cadastrar

--- backend/controllers/clientes.js
+++ backend/controllers/clientes.js
@@ -1,23 +1,19 @@
 const  express = require('express')
 const rota = express.Router()
-
 const db = require('./../db/models')
 
-
+// rota cadastrar cliente em http://localhost:8080/clientes
 rota.post('/clientes', async (requisicao, resposta) => {
-
     var dados = requisicao.body
     console.log(dados)
-
     await db.Clientes.create(dados).then((dadosCliente) => {
         return resposta.json({
-            mensagem: 'Cliente acessado!', dadosCliente
+            mensagem: 'Cliente Cadastrado!', dadosCliente
+        })
+    }).catch(() => {
+        return resposta.status(400).json({
+            mensagem: 'Cliente NÃO Cadastrado!'
         })
     })
-
-    
-
- 
 })
-
 module.exports = rota
