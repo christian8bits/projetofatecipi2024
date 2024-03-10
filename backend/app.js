9b52ac8ff21a1468981dc911f1a4b16559a07f1c
Christian Bits <christian8bits@gmail.com>
2024-03-10 01:43:33 -0300

    Cadastrar Cliente

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const cors = require('cors');
 //const db = require('./db/models')
 const app = express()
 const porta = 8080
 
 //criar middleware que recebe dados da requisicao 
 app.use(express.json()) // indica formato dos dados em json
+
+app.use((requisicao, resposta, next) => {
+    resposta.header('Access-Control-Allow-Origin', '*')
+    resposta.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    resposta.header('Access-Control-Allow-Headers', 'Content-Type')
+    app.use(cors())
+    next()
+})
 
 const clientes = require('./controllers/clientes')
 app.use('/', clientes)
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,6 @@
-import Head from "next/head";
-import { useState } from "react";
+import Head from "next/head"
+import { useState } from "react"
+import axios from "axios"
 
 export default function Home() {
 
@@ -7,9 +8,40 @@ export default function Home() {
     nome: '',
     email: ''
   })
-  
-  const valueInput = (evento) => setData({...data, [evento.target.nome]: evento.target.value})
 
+  const [message, setMessage] = useState('');
+  // recebe dados do formulario
+  const valueInput = (e) => setData({ ...data, [e.target.name]: e.target.value })
+
+  const addUser = async (e) => {
+    e.preventDefault() // bloqueia recarregamento da página
+    console.log('Chama API')
+    console.log("Cliente Nome: " + data.nome)
+    console.log("Cliente E-mail: " + data.email)
+    const headers = {
+      'headers': {
+        'Content-Type': 'application/json'
+      }
+    }
+    // requisição para o servidor 
+    await axios.post('http://localhost:8080/clientes', data, headers)
+      .then((response) => {
+        console.log(response.data.mensagem);
+        setMessage(response.data.mensagem);
+        // limpa os dados do state e os dados dos campos
+        setData({
+          nome: '',
+          email: ''
+        })
+      }).catch((err) => {
+        console.log(err.response.data.mensagem)
+        if (err.response) {
+          setMessage(err.response.data.mensagem)
+        } else {
+          setMessage("Erro: Conexão com servidor NÃO estabelecida, tentar novamente!")
+        }
+      })
+  }
 
   return (
     <>
@@ -20,25 +52,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
         <h2>Cadastrar Cliente</h2>
-        <form>
-          <label>Nome:</label>
-          <input type='text' name='nome' placeholder='Digite Nome' /><br/><br/>
+        {message ? <p>{message}</p> : ""}
 
+        <form onSubmit={addUser}>
+          <label>Nome:  </label>
+          <input type='text' name='nome' placeholder='Digite Nome' onChange={valueInput} value={data.nome} /> <br /><br />
           <label>E-Mail: </label>
-          <input type='email' name='email' placeholder='Digite E-Mail' /><br/><br/>
-
-          <button type='submit'>Salvar</button><br/><br/>
-
-
-
+          <input type='email' name='email' placeholder='Digite E-Mail' onChange={valueInput} value={data.email} /> <br /><br />
+          <button type='submit'>Salvar</button><br /><br />
         </form>
-
-
-
-
       </main>
     </>
-  );
+  )
 }
