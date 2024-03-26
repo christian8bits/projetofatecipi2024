8887cd81b063464a1c7609c4d90e575193791a34
christian8bits <christian8bits@gmail.com>
2024-03-26 18:35:39 -0300

    app

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const porta = 8080
-
+const app = express();
 //criar middleware que recebe dados da requisicao 
 app.use(express.json()) // indica formato dos dados em json
 
